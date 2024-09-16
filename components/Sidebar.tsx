844c0f32d6e62a1f316d54db1c1c7af3763023cb
wahabtlais <hussientlais20@gmail.com>
2024-09-16 21:03:00 +0300

    Finished signin and signup pages ui and functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const Sidebar = ({ user }: SiderbarProps) => {
 	const pathname = usePathname();
@@ -46,6 +47,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
 					);
 				})}
 			</nav>
+			<Footer user={user} />
 		</section>
 	);
 };
